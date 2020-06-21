b96495aa91e56c90e167210c05ba9130db9278e4
Andi Rady Kurniawan <andirady@kendra-systems.com>
2020-06-21 21:20:34 +0800

    Don't write to disk

--- app.py
+++ app.py
@@ -1,49 +1,30 @@
 import json
 import os
-import random
-from string import ascii_letters, digits
-from bottle import route, run, request, response, static_file
-from seqdiag.command import SeqdiagApp
-
-
-CHARS = ascii_letters + digits
-APP = SeqdiagApp()
-
-
-def random_string(n):
-    return ''.join(random.choice(CHARS) for i in range(n))
+from base64 import b64encode
+from bottle import route, run, request, static_file
+from seqdiag.builder import ScreenNodeBuilder
+from seqdiag.drawer import DiagramDraw
+from seqdiag.parser import parse_string
 
 
 @route('/')
 def index():
-    with open('public/index.html') as f:
-        response.set_cookie('sid', random_string(16))
-        return f.read()
+    return static_file('index.html', root='public')
 
 
 @route('/draw', method='POST')
 def draw():
-    sid = request.get_cookie('sid')
-    name = f'{random_string(8)}'
-    direc = f'tmp/{sid}'
-    if not os.path.isdir(direc):
-        os.makedirs(direc)
-    fn = f'{direc}/{name}.diag'
-    with open(fn, 'wb') as f:
-        body = request.body.read()
-        if len(body) == 0:
-            return json.dumps({ "error": { "code": 99 } })
-        f.write(body)
-    rc = APP.run([f"--size={request.query['w']}x{request.query['h']}", fn])
-    if rc != 0:
-        return json.dumps({ "error": { "code": rc } })
-    return json.dumps({ "src": f'/results/{name}.png' })
+    try:
+        tree = parse_string(request.body.read().decode('UTF-8'))
+    except Exception as e:
+        print("ERROR:", e)
+        return json.dumps({ "error": { "code": -1 } })
 
-
-@route('/results/<fn>', method='GET')
-def results(fn):
-    sid = request.get_cookie('sid')
-    return static_file(fn, root=os.path.join('tmp', sid))
+    diag = ScreenNodeBuilder.build(tree)
+    draw = DiagramDraw("png", diag)
+    draw.draw()
+    raw = draw.save()
+    return json.dumps({ "src": f"data:image/png;base64,{b64encode(raw).decode('UTF-8')}" })
 
 
 if __name__ == '__main__':
